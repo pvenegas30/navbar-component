ed1b92611443b841d44f9f50baf01d05ff427691
Pedro <pedrovenegasjob@gmail.com>
2023-05-24 03:49:40 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import NavBar from './NavBar';
+import './globals.css';
+
+export default function Home() {
+  return (
+    <main>
+    <div >
+      <NavBar />
+      
+    </div>
+    </main>
+  )
+}
